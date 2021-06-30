c319a0a0d252faec0eda030cdb2a2768c45ab079
Marco Gonçalves <marcomatiaspg@gmail.com>
2021-06-30 02:31:34 +0100

    Create Blank.vue

---
+++ leonardo2/src/views/Blank.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="pa-md-4 mx-lg-auto text-center " > 👷 Serviço em Desenvolvimento </h1>
+    </div>
+</template>
+ 
+<script>
+
+export default {
+    data() { return {}},
+    components:{
+    }
+}
+</script>
